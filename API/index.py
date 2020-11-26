f9204b84efb35942d18c8b812a22443cf762566c
yezarou <yezarou@gmail.com>
2020-11-26 21:14:56 +0100

    Modified API and added swagger file

--- API/index.py
+++ API/index.py
@@ -1,13 +1,15 @@
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications import imagenet_utils
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout, MaxPooling2D
 from PIL import Image
 import numpy as np
 import flask
 import io
 import cv2
+import queue
 
+callback_queue = queue.Queue()
 IMAGE_WIDTH=100
 IMAGE_HEIGHT=100
 IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
@@ -15,14 +17,33 @@ IMAGE_CHANNELS=3
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
-model = None
 
 def load_model1():
     # load the pre-trained Keras model (here we are using a model
     # pre-trained on ImageNet and provided by Keras, but you can
     # substitute in your own networks just as easily)
-    global model
-    model = load_model('./API/my_model.hdf5')
+    model = Sequential()
+    model.add(Conv2D(64, (3,3), activation='relu', strides=(2,2), input_shape=(IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_CHANNELS)))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(126, (3,3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(126, (3,3), activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(1, activation = 'sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    model.load_weights('./model.weights.best.hdf5')
+    return model
 
 def face_detection(img):
     face_cascade = cv2.CascadeClassifier('./API/haarcascade_frontalface_alt.xml')
@@ -45,19 +66,17 @@ def predict():
     if flask.request.method == "POST":
         if flask.request.files.get("image"):
             # read the image in PIL format
-            image = flask.request.files["image"].read()
-            image = Image.open(io.BytesIO(image))
+            #image = flask.request.files["image"].read()
+            #image = Image.open(io.BytesIO(image))
+            image = cv2.imdecode(np.fromstring(flask.request.files["image"].read(), np.uint8), cv2.IMREAD_UNCHANGED)
             image = np.array(image)
-            # preprocess the image and prepare it for classification
             image = face_detection(image)
             image = cv2.resize(image, (100,100))
             image = np.expand_dims(image, axis=0)
-
-            # classify the input image and then initialize the list
-            # of predictions to return to the client
+            model = load_model1()
             preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = preds
+            print(preds)
+            data["predictions"] = preds.tolist()[0][0]
 
             # # loop over the results and add them to the list of
             # # returned predictions
@@ -76,6 +95,5 @@ def predict():
 if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
         "please wait until server has fully started"))
-    load_model1()
     print("Done")
     app.run()
